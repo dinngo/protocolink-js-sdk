3fddfa4ccb4d3322f6178d677e35f59f12b6d2fb
ChouAndy <chouandy625@gmail.com>
2023-03-09 22:51:25 +0800

    feat: core add getPrice, getSupportedTokens interfaces

--- packages/core/src/logic.ts
+++ packages/core/src/logic.ts
@@ -2,10 +2,6 @@ import { IRouter } from './contracts/Router';
 import * as common from '@composable-router/common';
 import path from 'path';
 
-export interface LogicInstanceInterface {
-  getLogic(fields: object, options?: object): Promise<IRouter.LogicStruct>;
-}
-
 export abstract class Logic extends common.Web3Toolkit {
   static id: string;
   static protocol: string;
@@ -14,14 +10,18 @@ export abstract class Logic extends common.Web3Toolkit {
     return `${this.protocol}:${this.id}`;
   }
 
-  abstract getLogic(fields: object, options?: object): Promise<IRouter.LogicStruct>;
+  abstract getLogic(fields: any, options?: any): Promise<IRouter.LogicStruct>;
 }
 
-export abstract class ExchangeLogic extends Logic {
-  abstract getPrice(params: object): unknown;
+export interface LogicInterfaceGetSupportedTokens {
+  getSupportedTokens(): any;
 }
 
-export interface LogicInterface {
+export interface LogicInterfaceGetPrice {
+  getPrice(params: any): any;
+}
+
+export interface LogicClassInterface {
   new (...args: any[]): Logic;
   id: string;
   protocol: string;
@@ -30,7 +30,7 @@ export interface LogicInterface {
 }
 
 export function LogicDefinitionDecorator() {
-  return (logic: LogicInterface) => {
+  return (logic: LogicClassInterface) => {
     const [, , , logicFilePath] = common.getErrorStackCallerPaths();
     logic.id = path.basename(logicFilePath).split('.')[1];
     logic.protocol = path.basename(path.dirname(logicFilePath));
