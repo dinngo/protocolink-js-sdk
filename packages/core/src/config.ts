b63c0486bea52e48fb7c3a571323cb5a245c8039
ChouAndy <chouandy625@gmail.com>
2023-07-06 14:18:47 +0800

    feat: core package add polygon, arbitrum, zksync addresses

--- packages/core/src/config.ts
+++ packages/core/src/config.ts
@@ -4,32 +4,32 @@ type ContractNames = 'Router' | 'AgentImplementation';
 
 export const contractAddressMap: Record<number, Record<ContractNames, string>> = {
   [common.ChainId.mainnet]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '',
+    AgentImplementation: '',
   },
   [common.ChainId.polygon]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '0xdAAe399e32e3F6cE47cA96E3A5db09f65cAa575a',
+    AgentImplementation: '0x57643471E23430E2FD6334CBA6aEd85c5cc57281',
   },
   [common.ChainId.arbitrum]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '0xdAAe399e32e3F6cE47cA96E3A5db09f65cAa575a',
+    AgentImplementation: '0x57643471E23430E2FD6334CBA6aEd85c5cc57281',
   },
   [common.ChainId.optimism]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '',
+    AgentImplementation: '',
   },
   [common.ChainId.avalanche]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '',
+    AgentImplementation: '',
   },
   [common.ChainId.fantom]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '',
+    AgentImplementation: '',
   },
   [common.ChainId.zksync]: {
-    Router: '0x67e4d4Af097787Aa5a7daE7f9b147Bf32243F030',
-    AgentImplementation: '0xe495bDD1d9f74855D31BF881aA68b1bFeD728f7D',
+    Router: '0x7a750DdEA5efcd9140418515D260B917705F5D1b',
+    AgentImplementation: '0xfB79f9F8EC6EC0A891Eb87Ca4E1BeedfD8918a33',
   },
 };
 
--- packages/core/src/router.test.ts
+++ packages/core/src/router.test.ts
@@ -1,44 +1,43 @@
 import { calcAccountAgent } from './router';
 import * as common from '@protocolink/common';
 import { expect } from 'chai';
-import { setContractAddress } from './config';
 
 describe('Test newLogic', function () {
   const testCases = [
     {
-      chainId: common.ChainId.mainnet,
-      routerAddress: '0x30E0179f60FC9D3a30Ec195322ecEaeD37D2c4CD',
-      agentImplementationAddress: '0xCaeF6C1302bf6A6C19cc73A8500Eba2FC8FB664C',
+      chainId: common.ChainId.polygon,
       account: '0x5cb738DAe833Ec21fe65ae1719fAd8ab8cE7f23D',
-      expected: '0x0b3B738Ee870144575656768cA4A912809b969C5',
+      expected: '0xaDa591bBeD7f65E16911c676bA30f54e9a230eF5',
     },
     {
-      chainId: common.ChainId.mainnet,
-      routerAddress: '0x30E0179f60FC9D3a30Ec195322ecEaeD37D2c4CD',
-      agentImplementationAddress: '0xCaeF6C1302bf6A6C19cc73A8500Eba2FC8FB664C',
+      chainId: common.ChainId.polygon,
       account: '0xa3C1C91403F0026b9dd086882aDbC8Cdbc3b3cfB',
-      expected: '0x3371FB693DAE5C4b2AE8A0C9124a64cb7f38391a',
+      expected: '0x2C0e22b8d5070cb2500628E0bf43925a5c7b1559',
+    },
+    {
+      chainId: common.ChainId.arbitrum,
+      account: '0x5cb738DAe833Ec21fe65ae1719fAd8ab8cE7f23D',
+      expected: '0xaDa591bBeD7f65E16911c676bA30f54e9a230eF5',
+    },
+    {
+      chainId: common.ChainId.arbitrum,
+      account: '0xa3C1C91403F0026b9dd086882aDbC8Cdbc3b3cfB',
+      expected: '0x2C0e22b8d5070cb2500628E0bf43925a5c7b1559',
     },
     {
       chainId: common.ChainId.zksync,
-      routerAddress: '0xff4F3b46d68620FaBbBB788Ca2FE8759ac2a544E',
-      agentImplementationAddress: '0x6A76BBbd761110a4ff403Ce855d3ffb2Ca1bA74f',
       account: '0x5cb738DAe833Ec21fe65ae1719fAd8ab8cE7f23D',
-      expected: '0xF08498743E89deD67d44aD5952DaCF50FFEaCB34',
+      expected: '0xC055Cc1AFe2563C72A9b5fBbFc191730d19a7275',
     },
     {
       chainId: common.ChainId.zksync,
-      routerAddress: '0xff4F3b46d68620FaBbBB788Ca2FE8759ac2a544E',
-      agentImplementationAddress: '0x6A76BBbd761110a4ff403Ce855d3ffb2Ca1bA74f',
       account: '0xa3C1C91403F0026b9dd086882aDbC8Cdbc3b3cfB',
-      expected: '0xC62cAD7b6C2D1daD91abC9aD0DbaB3376C14eae2',
+      expected: '0x7Ae1cdB4cA3C4D3C753d3d7Dc1A8FAe3596332A0',
     },
   ];
 
-  testCases.forEach(({ chainId, routerAddress, agentImplementationAddress, account, expected }, i) => {
+  testCases.forEach(({ chainId, account, expected }, i) => {
     it(`case ${i + 1}`, function () {
-      setContractAddress(chainId, 'Router', routerAddress);
-      setContractAddress(chainId, 'AgentImplementation', agentImplementationAddress);
       const accountAgent = calcAccountAgent(chainId, account);
       expect(accountAgent).to.eq(expected);
     });
