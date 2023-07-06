e1b34d80788c3cd4ce43b982ec3621ffd1d92ff6
ChouAndy <chouandy625@gmail.com>
2023-07-06 14:35:41 +0800

    feat: test-helpers package fixtures add zksync tokens

---
+++ .changeset/calm-coins-call.md
@@ -0,0 +1,5 @@
+---
+'@protocolink/test-helpers': patch
+---
+
+fixtures add zksync tokens
--- packages/test-helpers/src/fixtures/tokens/index.ts
+++ packages/test-helpers/src/fixtures/tokens/index.ts
@@ -1,6 +1,11 @@
 import * as common from '@protocolink/common';
 import mainnetTokensJSON from './mainnet.json';
+import zksyncTokensJSON from './zksync.json';
 
 type MainnetTokenSymbols = keyof typeof mainnetTokensJSON;
 
 export const mainnetTokens = common.toTokenMap<MainnetTokenSymbols>(mainnetTokensJSON);
+
+type ZkSyncTokenSymbols = keyof typeof zksyncTokensJSON;
+
+export const zksyncTokens = common.toTokenMap<ZkSyncTokenSymbols>(zksyncTokensJSON);
