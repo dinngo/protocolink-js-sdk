be13015bef1b5438b216431fa6c20a81ecde5cfb
ChouAndy <chouandy625@gmail.com>
2023-07-24 12:54:13 +0800

    fix: src/router.test.ts error

--- packages/core/src/router.test.ts
+++ packages/core/src/router.test.ts
@@ -2,27 +2,27 @@ import { calcAccountAgent } from './router';
 import * as common from '@protocolink/common';
 import { expect } from 'chai';
 
-describe('Test newLogic', function () {
+describe('Test calcAccountAgent', function () {
   const testCases = [
     {
       chainId: common.ChainId.polygon,
       account: '0x5cb738DAe833Ec21fe65ae1719fAd8ab8cE7f23D',
-      expected: '0xaDa591bBeD7f65E16911c676bA30f54e9a230eF5',
+      expected: '0x23FDa9012F21FB03c1E0e271c20D4aA62d3A6923',
     },
     {
       chainId: common.ChainId.polygon,
       account: '0xa3C1C91403F0026b9dd086882aDbC8Cdbc3b3cfB',
-      expected: '0x2C0e22b8d5070cb2500628E0bf43925a5c7b1559',
+      expected: '0xa3Dcd65c3c2260d5c492174CDe2522AC12514223',
     },
     {
       chainId: common.ChainId.arbitrum,
       account: '0x5cb738DAe833Ec21fe65ae1719fAd8ab8cE7f23D',
-      expected: '0xaDa591bBeD7f65E16911c676bA30f54e9a230eF5',
+      expected: '0x23FDa9012F21FB03c1E0e271c20D4aA62d3A6923',
     },
     {
       chainId: common.ChainId.arbitrum,
       account: '0xa3C1C91403F0026b9dd086882aDbC8Cdbc3b3cfB',
-      expected: '0x2C0e22b8d5070cb2500628E0bf43925a5c7b1559',
+      expected: '0xa3Dcd65c3c2260d5c492174CDe2522AC12514223',
     },
     {
       chainId: common.ChainId.zksync,
