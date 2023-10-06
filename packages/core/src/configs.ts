12ea0d968edfbb0b120b99924b4aad259188379e
Jeff Huang <jeff@dinngo.co>
2023-10-06 15:14:54 +0800

    feat: core package add Router address for Optimism, Base, Metis and Avalanche network

--- packages/core/src/configs.ts
+++ packages/core/src/configs.ts
@@ -6,14 +6,26 @@ export const contractAddressMap: Record<number, Record<ContractNames, string>> =
   [common.ChainId.mainnet]: {
     Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
   },
+  [common.ChainId.optimism]: {
+    Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
+  },
   [common.ChainId.polygon]: {
+    Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
+  },
+  [common.ChainId.zksync]: {
+    Router: '0xa8350893B36e0425B50917125d9603F81F2D3C87',
+  },
+  [common.ChainId.metis]: {
+    Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
+  },
+  [common.ChainId.base]: {
     Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
   },
   [common.ChainId.arbitrum]: {
     Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
   },
-  [common.ChainId.zksync]: {
-    Router: '0xa8350893B36e0425B50917125d9603F81F2D3C87',
+  [common.ChainId.avalanche]: {
+    Router: '0x3fa3B62F0c9c13733245A778DE4157E47Cf5bA21',
   },
 };
 
