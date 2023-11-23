d9410788e45f6e51dfc4e2726f4513aee8093b06
Jeff Huang <jeff@dinngo.co>
2023-11-23 17:47:36 +0800

    feat: api package add Permit2 getPullTokenTokenList

--- packages/api/src/protocols/permit2/pull-token.ts
+++ packages/api/src/protocols/permit2/pull-token.ts
@@ -1,10 +1,15 @@
 import { Logic } from 'src/types';
 import * as common from '@protocolink/common';
+import { getProtocolTokenList } from 'src/api';
 import * as logics from '@protocolink/logics';
 
 export type PullTokenFields = common.Declasifying<logics.permit2.PullTokenLogicFields>;
 
 export type PullTokenLogic = Logic<PullTokenFields>;
+
+export async function getPullTokenTokenList(chainId: number): Promise<logics.permit2.PullTokenLogicTokenList> {
+  return getProtocolTokenList(chainId, logics.permit2.PullTokenLogic.rid);
+}
 
 export function newPullTokenLogic(fields: PullTokenFields): PullTokenLogic {
   return { rid: logics.permit2.PullTokenLogic.rid, fields };
