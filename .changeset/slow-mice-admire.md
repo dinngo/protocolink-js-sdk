d037bb9072f2c817334224cc66edad657d203460
Bob Lu <bob@dinngo.co>
2024-10-09 12:45:06 +0800

    fix(lending): update Aave V3 PoolDataProvider addresses

---
+++ .changeset/slow-mice-admire.md
@@ -0,0 +1,5 @@
+---
+'@protocolink/lending': patch
+---
+
+update Aave V3 PoolDataProvider addresses
--- packages/lending/src/protocols/aave-v3/configs.ts
+++ packages/lending/src/protocols/aave-v3/configs.ts
@@ -20,7 +20,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.mainnet,
     contractMap: {
       Pool: '0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2',
-      PoolDataProvider: '0x7B4EB56E7CD4b454BA8ff71E4518426369a138a3',
+      PoolDataProvider: '0x41393e5e337606dc3821075Af65AeE84D7688CBD',
       AaveOracle: '0x54586bE62E3c3580375aE3723C145253060Ca0C2',
     },
   },
@@ -29,7 +29,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.optimism,
     contractMap: {
       Pool: '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
-      PoolDataProvider: '0xd9Ca4878dd38B021583c1B669905592EAe76E044',
+      PoolDataProvider: '0x7F23D86Ee20D869112572136221e173428DD740B',
       AaveOracle: '0xD81eb3728a631871a7eBBaD631b5f424909f0c77',
     },
   },
@@ -38,7 +38,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.gnosis,
     contractMap: {
       Pool: '0xb50201558B00496A145fE76f7424749556E326D8',
-      PoolDataProvider: '0x501B4c19dd9C2e06E94dA7b6D5Ed4ddA013EC741',
+      PoolDataProvider: '0x57038C3e3Fe0a170BB72DE2fD56E98e4d1a69717',
       AaveOracle: '0xeb0a051be10228213BAEb449db63719d6742F7c4',
     },
   },
@@ -47,7 +47,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.polygon,
     contractMap: {
       Pool: '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
-      PoolDataProvider: '0x9441B65EE553F70df9C77d45d3283B6BC24F222d',
+      PoolDataProvider: '0x7F23D86Ee20D869112572136221e173428DD740B',
       AaveOracle: '0xb023e699F5a33916Ea823A16485e259257cA8Bd1',
     },
   },
@@ -56,7 +56,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.metis,
     contractMap: {
       Pool: '0x90df02551bB792286e8D4f13E0e357b4Bf1D6a57',
-      PoolDataProvider: '0x99411FC17Ad1B56f49719E3850B2CDcc0f9bBFd8',
+      PoolDataProvider: '0xC01372469A17b6716A38F00c277533917B6859c0',
       AaveOracle: '0x38D36e85E47eA6ff0d18B0adF12E5fC8984A6f8e',
     },
   },
@@ -65,7 +65,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.base,
     contractMap: {
       Pool: '0xA238Dd80C259a72e81d7e4664a9801593F98d1c5',
-      PoolDataProvider: '0x2d8A3C5677189723C4cB8873CfC9C8976FDF38Ac',
+      PoolDataProvider: '0xd82a47fdebB5bf5329b09441C3DaB4b5df2153Ad',
       AaveOracle: '0x2Cc0Fc26eD4563A5ce5e8bdcfe1A2878676Ae156',
     },
   },
@@ -74,7 +74,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.arbitrum,
     contractMap: {
       Pool: '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
-      PoolDataProvider: '0x6b4E260b765B3cA1514e618C0215A6B7839fF93e',
+      PoolDataProvider: '0x7F23D86Ee20D869112572136221e173428DD740B',
       AaveOracle: '0xb56c2F0B653B2e0b10C9b928C8580Ac5Df02C7C7',
     },
   },
@@ -83,7 +83,7 @@ export const configs: Config[] = [
     chainId: common.ChainId.avalanche,
     contractMap: {
       Pool: '0x794a61358D6845594F94dc1DB02A252b5b4814aD',
-      PoolDataProvider: '0x50ddd0Cd4266299527d25De9CBb55fE0EB8dAc30',
+      PoolDataProvider: '0x7F23D86Ee20D869112572136221e173428DD740B',
       AaveOracle: '0xEBd36016B3eD09D4693Ed4251c67Bd858c3c7C9C',
     },
   },
