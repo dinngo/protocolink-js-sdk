bc566ec94478aff443dac0dfef7262069387c1e8
Bob Lu <bob@dinngo.co>
2025-02-25 16:46:55 +0800

    chore(lending): update dependencies, support radiant v2 on Arbitrum, BNB

--- packages/lending/src/protocols/radiant-v2/configs.ts
+++ packages/lending/src/protocols/radiant-v2/configs.ts
@@ -23,10 +23,24 @@ export const configs: Config[] = [
     },
   },
   {
+    chainId: common.ChainId.bnb,
+    contractMap: {
+      ProtocolDataProvider: '0x499e336041202Cd4e55a1979e7511b3211033847',
+      PriceOracle: '0x0BB5c1Bc173b207cBf47CDf013617087776F3782',
+    },
+  },
+  {
     chainId: common.ChainId.base,
     contractMap: {
       ProtocolDataProvider: '0x07d2DC09A1CbDD01e5f6Ca984b060A3Ff31b9EAF',
       PriceOracle: '0xe373749cd9b2D379f7f6Dd595e5164498b922164',
+    },
+  },
+  {
+    chainId: common.ChainId.arbitrum,
+    contractMap: {
+      ProtocolDataProvider: '0xDd109cb6F2B2aEeBcE01727a31d99E3149aa7e41',
+      PriceOracle: '0xC0cE5De939aaD880b0bdDcf9aB5750a53EDa454b',
     },
   },
 ];
